997c1b1f99a5cb8929faf0823998bee0f65432dd
Utkarsh Pandey <utkarsh27a@gmail.com>
2019-01-04 23:46:48 +0530

    initial build

---
+++ precache-manifest.d486fe6e317b165bfb3fb040d85c56b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71e023b1163113886b2c",
+    "url": "/static/js/main.71e023b1.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "71e023b1163113886b2c",
+    "url": "/static/css/main.69dac852.chunk.css"
+  },
+  {
+    "revision": "51b182d0a91dd62023d1c8a83d9d34ac",
+    "url": "/index.html"
+  }
+];
